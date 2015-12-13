e2e2744d5f0fa821b75af5048196710e0a07090d
mambax7 <mambax7@gmail.com>
2015-12-13 16:52:46 -0500

    ver. 2.0

---
+++ xoops-styles/xoops_version.php
@@ -0,0 +1,14 @@
+<?php
+$modversion['name'] = 'xoops-styles';
+$modversion['version'] = 2.00;
+$modversion['description'] = 'Faux module Xoops Documentation';
+$modversion['credits'] = 'Chewb - Christian - Dugris - Kris_fr';
+$modversion['author'] = 'Burning & Xoops Design Team';
+$modversion['help'] = '';
+$modversion['license'] = 'GPL see LICENSE';
+$modversion['official'] = 1;
+$modversion['image'] = 'docs_slogo.png';
+$modversion['dirname'] = 'xoops-styles';
+
+// Menu
+$modversion['hasMain'] = 1;
